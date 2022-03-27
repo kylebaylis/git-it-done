68a2e1d553c8197b4f0c16c9057d7a6a54ce8ba2
Kyle <kylejmbaylis@gmail.com>
2022-03-27 13:54:12 -0400

    can make requests by switching user in function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,14 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+// user is there to pass a parameter through - ex that will add the input (kylebaylis in this case) into the user spot
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
 
-getUserRepos();
+getUserRepos("kylebaylis");
